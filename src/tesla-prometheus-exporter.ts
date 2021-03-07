04df6e49ecf02232f10ab043500eb77e1e8521b6
Pierpaolo Follia <pfollia@gmail.com>
2021-03-07 15:45:23 +0100

    Abandon external tesla-ts dependency

--- src/tesla-prometheus-exporter.ts
+++ src/tesla-prometheus-exporter.ts
@@ -1,7 +1,7 @@
 import client, { register } from 'prom-client';
 import express, { Express } from 'express';
 import yargs = require('yargs');
-import { TeslaAPI, VehicleAPI } from 'teslats';
+import { TeslaAPI, Vehicle } from './tesla-api';
 
 const DEFAULT_HTTP_PORT = 9898;
 const expr: Express = express();
@@ -16,6 +16,7 @@ interface ClientOptions {
   password?: string;
   port?: number;
   interval?: number;
+  vin?: string;
   debug?: boolean;
 }
 
@@ -26,208 +27,242 @@ const options: ClientOptions & any = yargs
     username: { alias: 'u', description: 'Tesla account username', demandOption: false },
     password: { alias: 'p', description: 'Tesla account password', demandOption: false },
     interval: { alias: 'i', description: 'Scraping interval in seconds', default: 120 },
+    vin: { description: 'VIN of the car to be monitored' },
     debug: { alias: 'd', description: 'Debug mode', boolean: true, default: false },
   })
   .help().argv;
 
 const metrics = {
-  battery_level: new client.Gauge({
-    name: 'tesla_battery_level',
-    help: 'Battery Level',
-    labelNames: ['vehicle'],
-  }),
+  charge_state: {
+    battery_level: new client.Gauge({
+      name: 'tesla_battery_level',
+      help: 'Battery Level',
+      labelNames: ['vehicle'],
+    }),
 
-  battery_range: new client.Gauge({
-    name: 'tesla_battery_range',
-    help: 'Battery Range',
-    labelNames: ['vehicle'],
-  }),
+    battery_range: new client.Gauge({
+      name: 'tesla_battery_range',
+      help: 'Battery Range',
+      labelNames: ['vehicle'],
+    }),
 
-  battery_heater_on: new client.Gauge({
-    name: 'tesla_battery_heater_on',
-    help: 'Battery Heater status',
-    labelNames: ['vehicle'],
-  }),
+    battery_heater_on: new client.Counter({
+      name: 'tesla_battery_heater_on',
+      help: 'Battery Heater status',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_current_request: new client.Gauge({
-    name: 'tesla_charge_current_request',
-    help: 'charge current request',
-    labelNames: ['vehicle'],
-  }),
+    charge_current_request: new client.Gauge({
+      name: 'tesla_charge_current_request',
+      help: 'charge current request',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_current_request_max: new client.Gauge({
-    name: 'tesla_charge_current_request_max',
-    help: 'charge current request max',
-    labelNames: ['vehicle'],
-  }),
+    charge_current_request_max: new client.Gauge({
+      name: 'tesla_charge_current_request_max',
+      help: 'charge current request max',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_energy_added: new client.Gauge({
-    name: 'tesla_charge_energy_added',
-    help: 'charge energy added',
-    labelNames: ['vehicle'],
-  }),
+    charge_energy_added: new client.Gauge({
+      name: 'tesla_charge_energy_added',
+      help: 'charge energy added',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_limit_soc: new client.Gauge({
-    name: 'tesla_charge_limit_soc',
-    help: 'charge limit soc',
-    labelNames: ['vehicle'],
-  }),
+    charge_limit_soc: new client.Gauge({
+      name: 'tesla_charge_limit_soc',
+      help: 'charge limit soc',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_limit_soc_max: new client.Gauge({
-    name: 'tesla_charge_limit_soc_max',
-    help: 'charge limit soc max',
-    labelNames: ['vehicle'],
-  }),
+    charge_limit_soc_max: new client.Gauge({
+      name: 'tesla_charge_limit_soc_max',
+      help: 'charge limit soc max',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_limit_soc_min: new client.Gauge({
-    name: 'tesla_charge_limit_soc_min',
-    help: 'charge limit soc min',
-    labelNames: ['vehicle'],
-  }),
+    charge_limit_soc_min: new client.Gauge({
+      name: 'tesla_charge_limit_soc_min',
+      help: 'charge limit soc min',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_limit_soc_std: new client.Gauge({
-    name: 'tesla_charge_limit_soc_std',
-    help: 'charge limit soc std',
-    labelNames: ['vehicle'],
-  }),
+    charge_limit_soc_std: new client.Gauge({
+      name: 'tesla_charge_limit_soc_std',
+      help: 'charge limit soc std',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_miles_added_ideal: new client.Gauge({
-    name: 'tesla_charge_miles_added_ideal',
-    help: 'charge miles added ideal',
-    labelNames: ['vehicle'],
-  }),
+    charge_miles_added_ideal: new client.Gauge({
+      name: 'tesla_charge_miles_added_ideal',
+      help: 'charge miles added ideal',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_miles_added_rated: new client.Gauge({
-    name: 'tesla_charge_miles_added_rated',
-    help: 'charge miles added rated',
-    labelNames: ['vehicle'],
-  }),
+    charge_miles_added_rated: new client.Gauge({
+      name: 'tesla_charge_miles_added_rated',
+      help: 'charge miles added rated',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_port_door_open: new client.Counter({
-    name: 'tesla_charge_port_door_open',
-    help: 'charge port door open',
-    labelNames: ['vehicle'],
-  }),
+    charge_port_door_open: new client.Counter({
+      name: 'tesla_charge_port_door_open',
+      help: 'charge port door open',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_rate: new client.Gauge({
-    name: 'tesla_charge_rate',
-    help: 'charge_rate',
-    labelNames: ['vehicle'],
-  }),
+    charge_rate: new client.Gauge({
+      name: 'tesla_charge_rate',
+      help: 'charge_rate',
+      labelNames: ['vehicle'],
+    }),
 
-  charge_to_max_range: new client.Gauge({
-    name: 'tesla_charge_to_max_range',
-    help: 'charge to max range',
-    labelNames: ['vehicle'],
-  }),
+    charge_to_max_range: new client.Counter({
+      name: 'tesla_charge_to_max_range',
+      help: 'charge to max range',
+      labelNames: ['vehicle'],
+    }),
 
-  charger_actual_current: new client.Gauge({
-    name: 'tesla_charger_actual_current',
-    help: 'charger actual current',
-    labelNames: ['vehicle'],
-  }),
+    charger_actual_current: new client.Gauge({
+      name: 'tesla_charger_actual_current',
+      help: 'charger actual current',
+      labelNames: ['vehicle'],
+    }),
 
-  charger_phases: new client.Gauge({
-    name: 'tesla_charger_phases',
-    help: 'charger_phases',
-    labelNames: ['vehicle'],
-  }),
+    charger_phases: new client.Gauge({
+      name: 'tesla_charger_phases',
+      help: 'charger_phases',
+      labelNames: ['vehicle'],
+    }),
 
-  charger_pilot_current: new client.Gauge({
-    name: 'tesla_charger_pilot_current',
-    help: 'charger pilot current',
-    labelNames: ['vehicle'],
-  }),
+    charger_pilot_current: new client.Gauge({
+      name: 'tesla_charger_pilot_current',
+      help: 'charger pilot current',
+      labelNames: ['vehicle'],
+    }),
 
-  charger_power: new client.Gauge({
-    name: 'tesla_charger_power',
-    help: 'charger_power',
-    labelNames: ['vehicle'],
-  }),
+    charger_power: new client.Gauge({
+      name: 'tesla_charger_power',
+      help: 'charger_power',
+      labelNames: ['vehicle'],
+    }),
 
-  charger_voltage: new client.Gauge({
-    name: 'tesla_charger_voltage',
-    help: 'charger_voltage',
-    labelNames: ['vehicle'],
-  }),
+    charger_voltage: new client.Gauge({
+      name: 'tesla_charger_voltage',
+      help: 'charger_voltage',
+      labelNames: ['vehicle'],
+    }),
 
-  charging_state: new client.Gauge({
-    name: 'tesla_charging_state',
-    help: 'charging state',
-    labelNames: ['vehicle'],
-  }),
+    charging_state: new client.Gauge({
+      name: 'tesla_charging_state',
+      help: 'charging state',
+      labelNames: ['vehicle'],
+    }),
 
-  est_battery_range: new client.Gauge({
-    name: 'tesla_est_battery_range',
-    help: 'est battery range',
-    labelNames: ['vehicle'],
-  }),
+    est_battery_range: new client.Gauge({
+      name: 'tesla_est_battery_range',
+      help: 'est battery range',
+      labelNames: ['vehicle'],
+    }),
 
-  ideal_battery_range: new client.Gauge({
-    name: 'tesla_ideal_battery_range',
-    help: 'ideal battery range',
-    labelNames: ['vehicle'],
-  }),
+    ideal_battery_range: new client.Gauge({
+      name: 'tesla_ideal_battery_range',
+      help: 'ideal battery range',
+      labelNames: ['vehicle'],
+    }),
 
-  max_range_charge_counter: new client.Gauge({
-    name: 'tesla_max_range_charge_counter',
-    help: 'max range charge counter',
-    labelNames: ['vehicle'],
-  }),
+    max_range_charge_counter: new client.Gauge({
+      name: 'tesla_max_range_charge_counter',
+      help: 'max range charge counter',
+      labelNames: ['vehicle'],
+    }),
 
-  time_to_full_charge: new client.Gauge({
-    name: 'tesla_time_to_full_charge',
-    help: 'time to full charge',
-    labelNames: ['vehicle'],
-  }),
+    time_to_full_charge: new client.Gauge({
+      name: 'tesla_time_to_full_charge',
+      help: 'time to full charge',
+      labelNames: ['vehicle'],
+    }),
 
-  timestamp: new client.Gauge({
-    name: 'tesla_timestamp',
-    help: 'timestamp',
-    labelNames: ['vehicle'],
-  }),
+    timestamp: new client.Gauge({
+      name: 'tesla_timestamp',
+      help: 'timestamp',
+      labelNames: ['vehicle'],
+    }),
 
-  usable_battery_level: new client.Gauge({
-    name: 'tesla_usable_battery_level',
-    help: 'usable battery level',
-    labelNames: ['vehicle'],
-  }),
+    usable_battery_level: new client.Gauge({
+      name: 'tesla_usable_battery_level',
+      help: 'usable battery level',
+      labelNames: ['vehicle'],
+    }),
+  },
+  drive_state: {
+    speed: new client.Gauge({
+      name: 'tesla_car_speed',
+      help: 'registered speed of the car',
+      labelNames: ['vehicle'],
+    }),
+  },
+  vehicle_state: {
+    odometer: new client.Gauge({
+      name: 'tesla_odometer',
+      help: 'run miles',
+      labelNames: ['vehicle'],
+    }),
+  },
 };
 
-async function reportVehicleData(api: TeslaAPI, vehicle: VehicleAPI) {
+async function reportVehicleData(api: TeslaAPI, vehicle: Vehicle): Promise<boolean> {
   try {
-    console.log('Requesting data');
-    const vehicleData = await vehicle.vehicleData();
-    if (options.debug) {
-      console.log(vehicleData.charge_state);
-    }
-
-    console.log('Expose data as metric');
-    Object.keys(metrics).forEach(metric => {
-      const value = vehicleData.charge_state[metric];
-      if (value !== undefined && value !== null && !(typeof value === 'string')) {
-        if (options.debug) {
-          console.log(`${metric}: ${value}`);
-        }
-        if (value === true || value === false) {
-          if (value) {
-            metrics[metric].inc(
-              {
-                vehicle: vehicleData.display_name,
-              },
-              1
-            );
-          }
-        } else {
-          metrics[metric].set(
-            {
-              vehicle: vehicleData.display_name,
-            },
-            value
-          );
-        }
+    if (vehicle.state !== 'asleep') {
+      console.log('Requesting data');
+      const vehicleData = await api.data(vehicle.id);
+      if (!vehicleData) {
+        console.warn(`Cannot read vehicle data for ${vehicle.display_name}`);
+        return false;
       }
-    });
+      if (options.debug) {
+        console.log(vehicleData);
+      }
+
+      console.log('Expose data as metric');
+      Object.keys(metrics).forEach(section => {
+        Object.keys(metrics[section]).forEach(metric => {
+          const value = vehicleData[section][metric];
+          if (value !== undefined && value !== null && !(typeof value === 'string')) {
+            const prometheusMetric = metrics[section][metric];
+            if (options.debug) {
+              console.log(`${prometheusMetric.name}: ${value}`);
+            }
+            try {
+              if (prometheusMetric instanceof client.Counter) {
+                if (value) {
+                  prometheusMetric.inc(
+                    {
+                      vehicle: vehicleData.display_name,
+                    },
+                    1
+                  );
+                }
+              } else {
+                prometheusMetric.set(
+                  {
+                    vehicle: vehicleData.display_name,
+                  },
+                  value
+                );
+              }
+            } catch (e) {
+              console.error(`Can't report metric ${prometheusMetric.name}: ${e.message}`);
+            }
+          }
+        });
+      });
+      return true;
+    } else {
+      console.info(`Vehicle ${vehicle.display_name} is ${vehicle.state}, skipping request`);
+      return false;
+    }
   } catch (e) {
     console.error(e.message);
   }
@@ -242,25 +277,32 @@ async function run() {
   try {
     const api = new TeslaAPI(options.token);
     const vehicles = await api.vehicles();
-    // get the first vehicle!
-    const vehicle = vehicles[0];
-    console.log(`Vehicle: ${vehicle.data.display_name}, state ${vehicle.data.state}`);
-    const timeout = setInterval(
-      async () => await reportVehicleData(api, vehicle),
-      options.interval * 1000
-    );
-
-    return new Promise<void>(resolve => {
-      process.stdin.on('keypress', async (str, key) => {
-        if (key.ctrl && key.name === 'c') {
-          clearInterval(timeout);
-          server.close();
-          resolve();
+    // get the selected vehicle or the first one!
+    let vehicle = options.vin ? vehicles.find(v => v.vin === options.vin) : vehicles[0];
+    if (vehicle) {
+      console.log(`Vehicle: ${vehicle.display_name}, state ${vehicle.state}`);
+      const ms = options.interval * 1000;
+      const timeout = setInterval(async () => {
+        const report = await reportVehicleData(api, vehicle);
+        if (!report) {
+          vehicle = await api.vehicle(vehicle.id);
         }
+      }, ms);
+
+      return new Promise<void>(resolve => {
+        process.stdin.on('keypress', async (str, key) => {
+          if (key.ctrl && key.name === 'c') {
+            clearInterval(timeout);
+            server.close();
+            resolve();
+          }
+        });
       });
-    });
+    } else {
+      console.error(`Can't find vehicle with VIN ${options.vin}`);
+    }
   } catch (e) {
-    console.error(e.message);
+    console.error(e.message, e);
     throw e;
   }
 }
